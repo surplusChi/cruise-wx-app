fb2319a9abfc8e1aec3cb40e7e17c96c5ba7032a
surplusChi <179696619@qq.com>
2023-07-17 22:54:55 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
